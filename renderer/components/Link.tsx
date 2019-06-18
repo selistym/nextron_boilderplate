5bb3f97b9512e1829b2706816ede5355bab35c99
selistym <selistym@protonmail.com>
2019-06-18 23:32:37 +0300

    init

---
+++ renderer/components/Link.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import clsx from 'clsx';
+import { withRouter } from 'next/router';
+import NextLink from 'next/link';
+import MuiLink from '@material-ui/core/Link';
+
+const NextComposed = React.forwardRef(function NextComposed(props: any, ref: any) {
+  const { as, href, prefetch, ...other } = props;
+
+  return (
+    <NextLink href={href} prefetch={prefetch} as={as}>
+      <a ref={ref} {...other} />
+    </NextLink>
+  );
+});
+
+function Link(props) {
+  const { activeClassName, router, className: classNameProps, innerRef, naked, ...other } = props;
+
+  const className = clsx(classNameProps, {
+    [activeClassName]: router.pathname === props.href && activeClassName,
+  });
+
+  if (naked) {
+    return <NextComposed className={className} ref={innerRef} {...other} />;
+  }
+
+  return <MuiLink component={NextComposed} className={className} ref={innerRef} {...other} />;
+}
+
+const RouterLink = withRouter(Link);
+
+export default React.forwardRef((props, ref) => <RouterLink {...props} innerRef={ref} />);
