24044591e2036744cf2225496e7d2ee6a476b2de
selistym <selistym@protonmail.com>
2019-06-19 23:38:59 +0300

    re--init

--- renderer/next.config.js
+++ renderer/next.config.js
@@ -1,13 +1,19 @@
-const withTypeScript = require('@zeit/next-typescript');
+require('dotenv').config();
+const withCSS = require('@zeit/next-css');
+const withTypescript = require('@zeit/next-typescript');
+const path = require('path');
+// eslint-disable-next-line
+const webpack = require('webpack');
 
-module.exports = withTypeScript({
-  webpack: config => Object.assign(config, {
-    target: 'electron-renderer',
+module.exports = withTypescript(
+  withCSS({
+    webpack(config) {
+      config.plugins.push(new webpack.EnvironmentPlugin(process.env));
+      // eslint-disable-next-line no-param-reassign
+      config.resolve.alias['@app'] = path.resolve('./');
+      // eslint-disable-next-line no-param-reassign
+      config.target = 'electron-renderer';
+      return config;
+    },
   }),
-  exportPathMap: async function () {
-    return {
-      '/home': { page: '/home' },
-      '/next': { page: '/next' },
-    };
-  },
-});
+);
